28ffc688ea5ea81695ead8c314f61be783a75e63
Юлия <prolhorenko_julia2001@mail.ru>
2022-12-23 11:26:03 +0700

    Вывод координат мыши

--- lab9.js
+++ lab9.js
@@ -28,3 +28,12 @@ function clickAction(button) {
         button.style.color = 'MediumAquamarine';
     }   
 }
+function showCoords(event) {
+    event.target.innerHTML = 'x = ' + event.offsetX + ', y = ' + event.offsetY + ' ' + 
+    (event.ctrlKey? '<b>Ctrl</b>': '<s>Ctrl</s>') + ' ' + (event.shiftKey? '<b>Shift</b>': '<s>Shift</s>') + ' ' + (event.altKey? '<b>Alt</b>': '<s>Alt</s>');
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    let coords = document.querySelector('#coords');
+    coords.addEventListener('mousemove', showCoords);
+})
